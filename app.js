fc59a3de8dea5b2bb1cca32ca13bb0255d5aae56
Kit <100928742+nvmmonkey@users.noreply.github.com>
2022-12-16 18:05:57 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,59 +1,78 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+//  IMPORT ///////
+const express = require("express")
+const bodyParser = require("body-parser")
+const request = require("request")
+const https = require("https")
+const { response } = require("express")
 
-app.get("/", (req, res) => res.type('html').send(html));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
+//  SETUP ///////
+app = express()
+app.use(express.static("public")) //Express Formula static folder for all the static files
+app.use(bodyParser.urlencoded({extended: true})) //Log input and Post in express.js
+port = 3000 //setup port
+apiKey = "aef7b1c7242b2bb423581fa4dda21ed9-us11"
+listId = "ba11850652"
+
+
+//  GET ///////
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+}) //sending the HTML
+
+
+
+//  POST ///////
+app.post("/", function(req, res){
+    const firstName = req.body.fName
+    const lastName = req.body.lName
+    const email = req.body.email
+    const data ={
+      members: [{
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+
+      }]
+    }
+    const addDataJson = JSON.stringify(data)
+    const options = {
+      url: "https://us11.api.mailchimp.com/3.0/lists/ba11850652",
+      method: "POST",
+      headers: {
+        Authorization: "auth aef7b1c7242b2bb423581fa4dda21ed9-us11"
+      },
+      body: addDataJson
+      
+    }
+    request (options, (error, response, body) => {
+      body = JSON.parse(body)
+      if(response.statusCode == 200){
+        res.sendFile(__dirname + "/success.html")
+        console.log(JSON.parse(addDataJson))
       }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+      else{
+        res.sendFile(__dirname + "/failure.html")
+        }
+    })
+  })
+
+app.post("/failure", function(req, res){
+  res.redirect("/")
+})
+
+app.post("/success", function(req, res){
+  res.redirect("/")
+})
+
+
+//  LISTEN ///////
+app.listen(process.env.port || port, function(req, res){
+    console.log("server is running on " + process.env.port)
+})
+
+
